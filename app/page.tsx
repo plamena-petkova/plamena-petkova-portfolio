b36d7de75f280c5e36b0cd4895127556854a4689
Plamena Petkova <pl.petkova@gmail.com>
2025-08-02 14:50:58 +0300

    Added scroll to top button

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,47 @@
-import UsePresenceData from "./animations/MotionCarousel";
+'use client'
 import Certificates from "./components/Certificates";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import ProjectCard from "./components/ProjectCard";
 import ProjectList from "./components/ProjectList";
 import Skills from "./components/Skills";
 import Timeline from "./components/Timeline";
+import { FaArrowUp } from "react-icons/fa";
+import { useInView, defaultFallbackInView } from "react-intersection-observer";
+
 
 export default function Home() {
+  const [ref, inView] = useInView({
+    threshold: 0,
+    initialInView: true,
+    fallbackInView: true,
+  });
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+
   return (
     <div className="bg-base-100">
-      <Navbar />
+      <div ref={ref}>
+        <Navbar />
+      </div>
+      {!inView && (
+        <button
+          onClick={scrollToTop}
+          className="btn btn-circle btn-primary fixed bottom-6 right-6 z-50 shadow-lg"
+        >
+          <FaArrowUp />
+        </button>
+      )}
       <Hero />
       <Skills />
       <ProjectList />
-      <Certificates/>
+      <Certificates />
       <Timeline />
       <Footer />
     </div>
+
   );
 }
